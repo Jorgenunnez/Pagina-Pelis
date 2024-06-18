de12cd4846e99b1328da8497fdf335a41b25327a
Jorgenunnez <jorge-alejandro-11@hotmail.com>
2024-06-18 00:41:02 -0300

    no me sale el navbar

--- src/Componentes/Navbar/Navbar.jsx
+++ src/Componentes/Navbar/Navbar.jsx
@@ -2,29 +2,21 @@ import estilos from "./Navbar.module.css";
 
 function Navbar() {
   return (
-    <div className={estilos.container}>
-      <div className={estilos.headerleft}>
+    <>
+      <navcontainer className={estilos.container}>
         <img src="" alt="Logo" className={estilos.logo} />
-      </div>
-      <div className={estilos.headermid}>
-        <nav>
-          <ul>
-            <li>
-              <a href="#">Peliculas</a>
-            </li>
-            <li>
-              <a href="#">Series</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <div className={estilos.headeright}>
-        <form role="search">
-          <input type="search" placeholder="Buscar" aria-label="search" />
-          <button>Buscar</button>
-        </form>
-      </div>
-    </div>
+        <div>
+          <a href="">Peliculas</a>
+          <a href="">Series</a>
+        </div>
+        <div>
+          <form action="">
+            <input type="search" placeholder="Buscar" aria-label="buscar" />
+            <button type="submit">buscar</button>
+          </form>
+        </div>
+      </navcontainer>
+    </>
   );
 }
 
